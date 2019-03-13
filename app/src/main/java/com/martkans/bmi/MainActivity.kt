2344c07fe85455052238e8db23c1e94c8f75fb32
Martin Kansik <martkans@outlook.com>
2019-03-13 19:28:25 +0100

    add option of counting bmi to main activity. first working version

--- app/src/main/java/com/martkans/bmi/MainActivity.kt
+++ app/src/main/java/com/martkans/bmi/MainActivity.kt
@@ -2,9 +2,12 @@ package com.martkans.bmi
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log.d
+import com.martkans.bmi.logic.Bmi
+import com.martkans.bmi.logic.BmiForKgCm
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +18,31 @@ class MainActivity : AppCompatActivity() {
         countBtn.setOnClickListener{
             val height = heightInput.text.toString()
             val mass = massInput.text.toString()
-            d("Height: ", height)
-            d("Mass: ", mass)
+
+            val df = DecimalFormat("#.##")
+            df.roundingMode = RoundingMode.HALF_EVEN
+            val bmi = BmiForKgCm(mass.toInt(), height.toInt())
+
+            yourBMITxtView.text = df.format(bmi.countBmi()).toString()
+            yourBMIrangeTxtView.text = bmiLevel(bmi)
 
         }
+    }
+
+
+    private fun bmiLevel(bmi: Bmi) : String{
+        val bmiVal = bmi.countBmi()
+
+        if (bmiVal < 18.5)
+            return "UNDERWEIGHT"
+        else if (bmiVal <= 24.9)
+            return "HEALTHY"
+        else if (bmiVal <= 29.9)
+            return "OVERWEIGHT"
+        else if (bmiVal <= 34.9)
+            return "OBESITY"
+        else
+            return "SEVERE OBESITY"
+
     }
 }
